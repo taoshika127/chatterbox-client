7ab8462edd814d9a3de8d94d458e1ecd385e8408
Sijia Tao <taoshika127@gmail.com>
2022-07-06 20:26:46 -0700

    Complete displaying messages from parse server

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -5,10 +5,16 @@ var MessageView = {
   // Learn more about Underscore's templating capability
   // here: https://underscorejs.org/#template.
   // TODO: Update this template accordingly.
-  render: _.template(`
-      <div class="chat">
-        <div class="username"><%- username %>: </div>
-        <div class="text"><%- text %></div>
-      </div>
-    `)
+  render: function(obj) {
+    var copyObj = JSON.parse(JSON.stringify(obj));
+    copyObj.text = _.escape(obj.text);
+    var compiled = _.template(`
+    <div class="chat">
+      <div class="username"><%- username %>: </div>
+      <div class="text"><%- text %></div>
+    </div>
+    `);
+    return compiled(copyObj);
+  }
+
 };
